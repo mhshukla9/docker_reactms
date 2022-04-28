8cea381be336b6d3c6e95ad2564fc3187f0d2995
Manish Shukla <manishshukla3107@gmail.com>
2022-04-28 17:20:15 -0400

    seven commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Honey welcome  Test reload again.
+          Edit <code>src/App.js</code> Manish AWS welcome  Test reload again.
         </p>
         <a
           className="App-link"
